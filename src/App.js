655594d8f1be75286c0f8fc6ec3dedf13b2200b3
Juliana Ifionu <julieifionu@gmail.com>
2021-09-27 10:41:22 +0100

    Add Router to display nav links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './Components/Navigation';
+import Books from './Redux/Books/Books';
+import Categories from './Redux/Categories/Categories';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <switch>
+      <Switch>
         <Route exact path="/">
-          <h1>Hello</h1>
+          <Books />
         </Route>
-        <Route path="/C">
-          <h1>Hello</h1>
+        <Route path="/categories">
+          <Categories />
         </Route>
-      </switch>
+      </Switch>
     </div>
   );
 }
