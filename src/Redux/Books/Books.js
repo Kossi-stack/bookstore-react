0c5665f884921d1faacb90e173ed62f52d2c861b
Juliana Ifionu <julieifionu@gmail.com>
2021-09-27 16:39:30 +0100

    Create initial state of the app

--- src/Redux/Books/Books.js
+++ src/Redux/Books/Books.js
@@ -1,5 +1,16 @@
-const Books = () => (
-  <h1>Books components</h1>
-);
+const ADD_BOOK = 'bookStore/Books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/Books/REMOVE_BOOK';
+
+const initialState = [
+  {
+    arthur: 'Juliana',
+    title: 'Think and code',
+  },
+  {
+    arthur: 'May',
+    title: 'Think and walk',
+  },
+];
+
 
 export default Books;
