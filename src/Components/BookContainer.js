1f3072cf1fb4fe6580305b33df246d7c58ea2aa8
Juliana Ifionu <julieifionu@gmail.com>
2021-09-28 09:22:42 +0100

    Add reducer and passed initial state

--- src/Components/BookContainer.js
+++ src/Components/BookContainer.js
@@ -1,42 +1,47 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook, removeBook } from '../Redux/Books/Books';
 import InputBooks from './InputBook';
 import BookList from './BookList';
 
-const initialState = [
-  {
-    author: 'Juliana',
-    title: 'Think and code',
-    id: 1,
-  },
-  {
-    author: 'May',
-    title: 'Think and walk',
-    id: 2,
-  },
-];
+// const initialState = [
+//   {
+//     author: 'Juliana',
+//     title: 'Think and code',
+//     id: 1,
+//   },
+//   {
+//     author: 'May',
+//     title: 'Think and walk',
+//     id: 2,
+//   },
+// ];
+
+const dispatch = useDispatch();
 
 const BookContainer = () => {
-  const [book, setBooks] = useState(initialState);
+  // const [book, setBooks] = useState(initialState);
 
-  const addBook = (title, author) => {
+  const addNewBook = (title, author) => {
     const newBook = {
       id: uuidv4(),
       title,
       author,
     };
 
-    setBooks([...book, newBook]);
+    dispatch(addBook(newBook));
+    // setBooks([...book, newBook]);
   };
 
-  const removeBook = (id) => {
-    setBooks([...book.filter((book) => book.id !== id)]);
+  const deleteBook = (id) => {
+    dispatch(removeBook(id));
   };
 
   return (
     <div>
-      <BookList books={book} removeBookProps={removeBook} />
-      <InputBooks addBookProps={addBook} />
+      <BookList books={book} removeBookProps={deleteBook} />
+      <InputBooks addBookProps={addNewBook} />
     </div>
   );
 };
