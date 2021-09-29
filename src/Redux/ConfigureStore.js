c8ee9177b51abff43eba4326be1f3dba717b61d6
Juliana Ifionu <julieifionu@gmail.com>
2021-09-29 07:31:20 +0100

    Install thunk and import thunk then add it as an argument to applyMiddlewaree

--- src/Redux/ConfigureStore.js
+++ src/Redux/ConfigureStore.js
@@ -1,11 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
 import booksReducer from './Books/Books';
 
 const reducer = combineReducers({
   booksReducer,
 });
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(
+  reducer,
+  // applyMiddleware(logger),
+  applyMiddleware(thunk),
+);
 
 export default store;
